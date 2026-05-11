75da3d25446d7e67847d1a5e77d5aefcfa472c45
eman <emansajjad863@gmail.com>
2026-05-11 08:59:40 -0700

    fixed mistake

--- next.config.mjs
+++ next.config.mjs
@@ -9,12 +9,7 @@ const withPWA = withPWAInit({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental:{
-    turbo:{
-      enabled:false,
-
-    },
-  },
+  
 
 
 };
