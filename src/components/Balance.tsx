65b0e72a73355aecbdf6c41246798678913701bf
eman <emansajjad863@gmail.com>
2026-05-12 02:27:08 -0700

    UI: compact balance and optimized layout for small screens

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -11,7 +11,7 @@ export default function Balance(){
     return(
         <div className="mb-6 text-center">
             <h3 className=" uppercase text-gray-600 font-semibold p-6 text-sm">Your Balance</h3>
-            <h1 className="text-4xl font-bold text-gray-900 ">${totalBalance}</h1>
+            <h1 className="text-2xl font-bold text-gray-900 ">${totalBalance}</h1>
         </div>
     )
 }
